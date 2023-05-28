cf6072ba8398b7ba0314b90206d16fea804eb3fc
HuuLong <longanhminh9@gmail.com>
2023-05-29 00:56:55 +0700

    part2

--- app/src/main/java/com/greenhuecity/view/navfragment/SettingFragment.java
+++ app/src/main/java/com/greenhuecity/view/navfragment/SettingFragment.java
@@ -15,7 +15,7 @@ public class SettingFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.activity_settings,container,false);
         return view;
     }
 }
